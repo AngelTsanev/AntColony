ec6c362ab25f3e23cadbbc39b8e2f7db9e603abf
Angel Tsanev <angeltzanev@gmail.com>
2016-04-10 22:13:13 +0300

    change ant color

--- lib/simulator.py
+++ lib/simulator.py
@@ -7,7 +7,7 @@ import time
 
 
 Matrix = RGBMatrix(32, 2, 1)
-Matrix.pwmBits = 11
+Matrix.pwmBits = 5
 Matrix.brightness = 50
 
 def colorize(foods, base, ants):
@@ -15,7 +15,7 @@ def colorize(foods, base, ants):
         Matrix.SetPixel(food[0], food[1], 0, 200, 0)
 
     for ant in ants:
-        Matrix.SetPixel(ant[0], ant[1], 255, 255, 0)
+        Matrix.SetPixel(ant[0], ant[1], 200, 0, 0)
 	#base
     Matrix.SetPixel(base[0], base[1], 100, 255, 100)
 
@@ -25,7 +25,7 @@ field = Field("field.txt", (30, 40))
 field.food.append((10, 10))
 field.food.append((20, 20))
 
-for i in range(1, 30):
+for i in range(1, 11):
     field.units.append(Ant(Color(0, 0, 100), 30, 40, i))
     field.add_occupied((30, 40))
 
@@ -41,6 +41,6 @@ while True:
     if i % 10 == 0:
         i = 1
         field.evaporate_pheromones()
-    time.sleep(0.4)
+    time.sleep(1)
     offscreenCanvas = Matrix.SwapOnVSync(offscreenCanvas)
 
