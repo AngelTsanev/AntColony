fb631eb9b2888680e6c26e4e75cd703df91d7d5c
AngelTsanev <angeltzanev@gmail.com>
2016-04-10 21:50:41 +0300

    adding led matrix logic

---
+++ lib/simulator.py
@@ -0,0 +1,38 @@
+from rgbmatrix import RGBMatrix
+from rgbmatrix import graphics
+
+
+Matrix = RGBMatrix(32, 2, 1)
+Matrix.pwmBits = 11
+Matrix.brightness = 50
+
+def colorize(foods, base, ants):
+    for food in foods:
+        Matrix.SetPixel(food[0], food[1], 0, 200, 0)
+
+    for ant in ants:
+        Matrix.SetPixel(ant[0], ant[1], 255, 255, 0)
+	#base
+    Matrix.SetPixel(base[0], base[1], 100, 255, 100)
+
+
+field = Field("field.txt", (30, 40))
+
+field.food.append((10, 10))
+field.food.append((20, 20))
+
+for i in range(1, 30):
+    field.units.append(Ant(Color(0, 0, 100), 30, 40, i))
+    field.add_occupied((30, 40))
+
+i = 1
+while True:
+    i+=1
+    field.print()
+    colorize(field.food, field.base, field.occupied)
+    field.generate_next()
+    if i % 10 == 0:
+        i = 1
+        field.evaporate_pheromones()
+    time.sleep(0.4)
+
